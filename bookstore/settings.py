69562748802e2177331c708128adccac3fede0b4
narvas12 <ebukaemmanuel71@gmail.com>
2023-07-29 09:08:24 -0400

    cart

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -66,6 +66,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'bookstoreApp.content_processor.categories',
+                'cart.content_processor.cart',
             ],
         },
     },
@@ -121,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR / 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
